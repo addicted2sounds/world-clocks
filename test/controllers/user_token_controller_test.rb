876341525891d5a8d1e314924d0c74121196e942
Oleg <addicted2sounds@gmail.com>
2016-06-04 12:14:13 +0300

    user token controller test

---
+++ test/controllers/user_token_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class UserTokenControllerTest < ActionDispatch::IntegrationTest
+  describe 'POST /user_token' do
+    test 'the truth' do
+      assert true
+    end
+  end
+end
