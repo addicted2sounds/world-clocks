d27ae441dc2d7b0d44cea0f1dd53646a97158d5f
Oleg <addicted2sounds@gmail.com>
2016-06-05 11:27:42 +0300

    timezone request spec

--- spec/requests/timezones_spec.rb
+++ spec/requests/timezones_spec.rb
@@ -10,12 +10,11 @@ RSpec.describe 'Timezones', :type => :request do
     end
 
     context 'when validated' do
-      before :each do
-        get timezones_path, headers: auth_headers(user)
-      end
+      let(:request) { get timezones_path, headers: auth_headers(user) }
 
       context 'when no timezones defined' do
         it 'returns empty array' do
+          request
           expect(json['data']).to eq []
         end
       end
@@ -24,9 +23,17 @@ RSpec.describe 'Timezones', :type => :request do
         before(:each) { create :timezone, user: user }
 
         it 'has timezone' do
+          request
           expect(json['data'].count).to eq 1
         end
       end
+    end
+  end
+
+  describe 'POST /timezones' do
+    it 'fails without authentication' do
+      post timezones_path
+      expect(response).to have_http_status(401)
     end
   end
 end
