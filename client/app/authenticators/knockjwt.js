97a0759f58b4d40db44e8e3f08b4dc18a1d25b8d
Oleg <addicted2sounds@gmail.com>
2016-06-05 17:01:24 +0300

    added authorizer and authenticator

---
+++ client/app/authenticators/knockjwt.js
@@ -0,0 +1,58 @@
+import Ember from 'ember';
+import Base from 'ember-simple-auth/authenticators/base';
+import config from '../config/environment';
+
+const { RSVP: { Promise }, $: { ajax }, run } = Ember;
+
+export default Base.extend({
+  tokenEndpoint: `${config.host}/knock/auth`,
+
+  restore(data) {
+    return new Promise((resolve, reject) => {
+      if (Ember.isEmpty(data.token)) {
+        resolve(data);
+      } else {
+        reject();
+      }
+    });
+  },
+
+  authenticate(creds) {
+    const { identification, password } = creds;
+
+    const data = JSON.stringify({
+      auth: {
+        email: identification,
+        password
+      }
+    });
+
+    const requestOpts = {
+      url: this.tokenEndpoint,
+      type: 'POST',
+      data,
+      contentType: 'application/json',
+      dataType: 'json'
+    };
+
+
+    return new Promise((resolve, reject) => {
+      ajax(requestOpts).then((response) => {
+        const { jwt } =  response;
+        run(() => {
+          resolve({
+            token: jwt
+          });
+        });
+      }, (error) => {
+        run(() => {
+          reject(error);
+        });
+      });
+    });
+  },
+
+  invalidate(data) {
+    return Promise.resolve(data);
+  }
+});
