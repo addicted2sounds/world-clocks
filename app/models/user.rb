0db44c2db5c848ecad51fe1f7b107c634199aaa3
Oleg <addicted2sounds@gmail.com>
2016-06-09 13:01:16 +0300

    fixed issue with token in specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,8 @@ class User < ApplicationRecord
   has_many :timezones
   validates :email, email: true, uniqueness: true
 
-  # def self.from_token_request(request)
-  #   email = request.params['auth'] && request.params['auth']['email']
-  #   self.find_by email: email
-  # end
-
   def self.from_token_payload(payload)
-    self.find_by email: payload['email']
+    self.find payload['sub']
   end
 
   def can_manage_alias_timezones?
