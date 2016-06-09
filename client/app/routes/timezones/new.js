520a665e319b3bfed57840ff880dd97742b71c51
Oleg <addicted2sounds@gmail.com>
2016-06-09 08:53:49 +0300

    new timezone

--- client/app/routes/timezones/new.js
+++ client/app/routes/timezones/new.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
-    save(timezone) {
+    saved(timezone) {
       timezone.save().then(() => {
         this.transitionTo('timezones.index');
       }).catch(() => {
