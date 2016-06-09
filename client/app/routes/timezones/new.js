f17a2209a862f8b0a274c24d3b3307af9ea2766e
Oleg <addicted2sounds@gmail.com>
2016-06-09 13:42:18 +0300

    authorize user when viewing

--- client/app/routes/timezones/new.js
+++ client/app/routes/timezones/new.js
@@ -4,4 +4,10 @@ export default Ember.Route.extend({
   model() {
     return this.store.createRecord('timezone');
   },
+  actions: {
+    willTransition() {
+      let timezone = this.controller.get('model');
+      timezone.destroyRecord();
+    }
+  }
 });
--- client/app/routes/users/new.js
+++ client/app/routes/users/new.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  //model() {
-    //return this.store.createRecord('user');
-  //},
+  model() {
+    return this.store.createRecord('user');
+  },
   actions: {
     willTransition() {
       let user = this.controller.get('model');
-      user.rollback();
+      user.destroyRecord();
     }
   }
 });
