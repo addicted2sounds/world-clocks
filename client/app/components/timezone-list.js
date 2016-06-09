654d0ffcfd2285adfe2837fef899c77220837a28
Oleg <addicted2sounds@gmail.com>
2016-06-09 09:21:29 +0300

    remove timezone link

--- client/app/components/timezone-list.js
+++ client/app/components/timezone-list.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(timezone) {
+      timezone.destroyRecord().then(() => {
+        console.log('Timezone deleted');
+      }).catch(() => {
+        console.log('Error deleting timezone');
+      });
+    }
+  }
 });
