1003760e8a05bd59c0ebad1138b6214e4b7e42ba
Oleg <addicted2sounds@gmail.com>
2016-06-10 21:07:55 +0300

    migrated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,17 @@ class ApplicationController < ActionController::API
   include Pundit
   after_action :verify_authorized, except: :index
   after_action :verify_policy_scoped, only: :index
+
+  def permitted_params(record)
+    policy = policy(record)
+    attributes = policy.send(:permitted_attributes)
+    params.require(:data).require(:attributes).permit(attributes)
+  end
+
+  def respond_with_errors(object)
+    render json: object,
+           status: :unprocessable_entity,
+           adapter: :json_api,
+           serializer: ActiveModel::Serializer::ErrorSerializer
+  end
 end
